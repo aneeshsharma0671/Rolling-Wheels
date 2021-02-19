ee43613e85547030163b719eb2231dfd4ff65dbb
Aneesh sharma <aneeshsharma0671@gmail.com>
2021-02-19 21:39:32 +0530

    Basic Game Mechanics

---
+++ Rolling Wheels/Assets/Scripts/TrackGeneration.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackGeneration : MonoBehaviour
+{
+    public GameObject[] trackprefabs;
+    public List<GameObject> tracks;
+    public Transform Wheel;
+    private float Anchor = 0;
+    public float number_of_tiles;
+
+    public void Start()
+    {
+        for (int i = 0; i < number_of_tiles; i++)
+        {
+           
+                GenerateTrack();
+            
+        }
+    }
+
+    public void Update()
+    {
+        if (Wheel.position.z - 100 > Anchor - (number_of_tiles * 100))
+        {
+            GenerateTrack();
+            DestroyTrack();
+        }
+        
+    }
+
+    void GenerateTrack()
+    {
+        GameObject Track = Instantiate(trackprefabs[0]);
+        Track.transform.position = new Vector3(0,0,Anchor);
+        tracks.Add(Track);
+        Anchor += 100;
+    }
+
+    void DestroyTrack()
+    {
+        Destroy(tracks[0]);
+        tracks.RemoveAt(0);
+    }
+}
---
+++ Rolling Wheels/Assets/Scripts/WheelController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class WheelController : MonoBehaviour
+{
+    private Rigidbody wheel_rb;
+    private CapsuleCollider wheel_collider;
+    public GameObject[] wheel_Spokes;
+    public bool IsPressed = false;
+
+    public void Awake()
+    {
+        wheel_rb = GetComponent<Rigidbody>();
+        wheel_collider = GetComponent<CapsuleCollider>();
+    }
+
+    public void Update()
+    {
+        wheel_rb.angularVelocity = transform.right * 10;
+    }
+
+    public void FixedUpdate()
+    {
+        if(wheel_collider.radius > 1 && !IsPressed)
+        {
+            wheel_collider.radius -= Time.fixedDeltaTime;
+            wheel_Spokes[0].transform.localScale = new Vector3(0.25f, (wheel_collider.radius - 1) / 2, 0.25f);
+            wheel_Spokes[1].transform.localScale = new Vector3(0.25f, (wheel_collider.radius - 1) / 2, 0.25f);
+            wheel_Spokes[2].transform.localScale = new Vector3(0.25f, (wheel_collider.radius - 1) / 2, 0.25f);
+            wheel_Spokes[3].transform.localScale = new Vector3(0.25f, (wheel_collider.radius - 1) / 2, 0.25f);
+
+        }
+        if (IsPressed)
+        {
+            wheel_collider.radius += Time.fixedDeltaTime;
+            wheel_Spokes[0].transform.localScale = new Vector3(0.25f, (wheel_collider.radius - 1) / 2, 0.25f);
+            wheel_Spokes[1].transform.localScale = new Vector3(0.25f, (wheel_collider.radius - 1) / 2, 0.25f);
+            wheel_Spokes[2].transform.localScale = new Vector3(0.25f, (wheel_collider.radius - 1) / 2, 0.25f);
+            wheel_Spokes[3].transform.localScale = new Vector3(0.25f, (wheel_collider.radius - 1) / 2, 0.25f);
+        }
+    }
+
+    public void Input(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+        {
+            IsPressed = true;
+        }
+        if(context.canceled)
+        {
+            IsPressed = false;
+        }
+       
+    }
+
+}
+
